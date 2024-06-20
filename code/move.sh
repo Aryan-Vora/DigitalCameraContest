b67240bd34462c720fe65c14f50d6093d9b2f1d8
Aryan Vora <arvora@ucsc.edu>
2024-06-19 17:31:21 -0700

    edited scripts to only take in a folder

--- code/move.sh
+++ code/move.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-# Check if file extension is provided
+# Check if directory is provided
 if [ -z "$1" ]
 then
-    echo "Please provide a file extension."
+    echo "Please provide a directory."
     exit 1
 fi
 
-# Create the directory if it doesn't exist
-mkdir -p "$1"
-
-# Move all files with the provided extension to the corresponding directory
-for file in *."$1"
+# Iterate over all files in the provided directory
+for file in "$1"/*
 do
-    mv "$file" "$1"/
-done
+    # Get the file extension
+    ext="${file##*.}"
 
+    # Create the directory for this file type if it doesn't exist
+    mkdir -p "$1/$ext"
+
+    # Move the file to the corresponding directory
+    mv "$file" "$1/$ext/"
+done
--- code/onefileextension.sh
+++ code/onefileextension.sh
@@ -10,12 +10,22 @@ fi
 # Iterate over all files in the provided directory
 for file in "$1"/*
 do
+    echo "Processing file: $file"
+
     # Get the base name of the file without path
     base=$(basename "$file")
+    echo "Base name of the file: $base"
     
     # Remove all extensions except the last one
     newname="${base%%.*}.${base##*.}"
+    echo "New name for the file: $newname"
     
-    # Rename the file
-    mv -- "$file" "${file%/*}/$newname"
+    # Rename the file only if new name is different
+    if [ "$newname" != "$base" ]
+    then
+        echo "Renaming $file to $newname"
+        mv -- "$file" "${file%/*}/$newname"
+    else
+        echo "Skipping $file as it only has one extension"
+    fi
 done
